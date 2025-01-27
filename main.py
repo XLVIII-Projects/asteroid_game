005609c9782737a4c5673806e176f53c982c94c4
Nielzo <nielsbleeker@gmail.com>
2025-01-27 17:21:31 +0100

    added cicleshape and player functions

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from player import Player
+from circleshape import CircleShape
 
 
 def main():
     pygame.init()
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock() #create a clock object
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-        
+
         screen.fill((0, 0, 0))
-        print("Starting asteroids!")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
+
+        player.draw(screen)
+
+        dt = clock.tick(60)/1000 #get the time in seconds since the last frame
+
         pygame.display.flip()
 
 if __name__ == "__main__":
