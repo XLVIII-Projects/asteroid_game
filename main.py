2361f4e7d78a3e44e045304d5ebd3171ec17aea6
Nielzo <nielsbleeker@gmail.com>
2025-01-28 12:00:53 +0100

    player triangle added + basic movement

--- main.py
+++ main.py
@@ -8,17 +8,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock() #create a clock object
-    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+        
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    dt = 0    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-
+        
+        for updatable_object in updatable:
+            updatable_object.update(dt)
+        
         screen.fill((0, 0, 0))
 
-        player.draw(screen)
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
 
         dt = clock.tick(60)/1000 #get the time in seconds since the last frame
 
--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         # self.position = pygame.Vector2(x, y)
         self.rotation = 0
+        # self.add(*self.containers)
     
     def draw(self, screen):
             pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -18,5 +19,25 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def rotate(self, dt):
+         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+        
     
     
